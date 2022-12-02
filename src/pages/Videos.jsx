3779da14ddf026bd10db832c2e53bf44517e6ed8
kyungbaa <cocoroto@naver.com>
2022-12-02 19:39:23 +0900

    FEATURE: 검색 API 사용 및 context Api로 인스턴스 한번만 생성하도록 수정

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,18 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import VideoCard from '../components/VideoCard';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
 export default function Videos() {
   const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
   const {
     isLoading,
     error,
     data: videos,
-  } = useQuery(['videos', keyword], async () => {
-    return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
-      .then((res) => res.json())
-      .then((data) => data.items);
-  });
+  } = useQuery(['videos', keyword], () => youtube.search(keyword));
 
   return (
     <>
