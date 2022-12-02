31e55ac001febe3df07eff46cd2ca804f8d881e9
kyungbaa <cocoroto@naver.com>
2022-12-02 21:43:02 +0900

    STYLE: 마우스오버 영상 설명

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { formatAgo } from '../util/date';
 
 export default function VideoCard({ video }) {
-  const { channelTitle, liveBroadcastContent, title, thumbnails, publishedAt } =
+  const { channelTitle, description, title, thumbnails, publishedAt } =
     video.snippet;
+  const [descriptionOn, setDescriptionOn] = useState(false);
+
+  const onDescription = () => {
+    setDescriptionOn(true);
+  };
+
+  const outDescription = () => {
+    setDescriptionOn(false);
+  };
 
   return (
     <li>
-      <img
-        src={thumbnails.medium.url}
-        alt={title}
-        className="w-full rounded-2xl"
-      />
+      <div
+        className="w-full relative"
+        onMouseEnter={onDescription}
+        onMouseLeave={outDescription}
+      >
+        <img
+          src={thumbnails.medium.url}
+          alt={title}
+          className="w-full rounded-2xl"
+        />
+        {descriptionOn && (
+          <div className="bg-black/80 w-full h-full absolute top-0 p-5 rounded-2xl ">
+            <p className="line-clamp-5">{description}</p>
+          </div>
+        )}
+      </div>
       <div className="px-3">
         <p className="text-sm font-semibold my-2 line-clamp-2">{title}</p>
         <p className="text-xs opacity-80">{channelTitle}</p>
